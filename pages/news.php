e97555920735ae2bec22d4e2b6ad28577efef2fc
kazakh <166235011+localhelp@users.noreply.github.com>
2025-02-02 19:36:52 +0100

    Create news.php

---
+++ pages/news.php
@@ -0,0 +1,13 @@
+<?php include '../includes/header.php'; ?>
+<?php include '../includes/navbar.php'; ?>
+
+<div class="container">
+    <h1>Latest News</h1>
+    <p>Stay updated on the latest operations and developments in the world of special forces.</p>
+    <ul>
+        <li>February 2025: DEVGRU successfully neutralizes a threat in Eastern Europe.</li>
+        <li>January 2025: SEAL Team Six involved in high-profile hostage rescue.</li>
+    </ul>
+</div>
+
+<?php include '../includes/footer.php'; ?>
