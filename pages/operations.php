0a39c71896bbe18ba9d4e3b7fedc8d1ed9878908
kazakh <166235011+localhelp@users.noreply.github.com>
2025-02-02 19:36:20 +0100

    Create operations.php

---
+++ pages/operations.php
@@ -0,0 +1,14 @@
+<?php include '../includes/header.php'; ?>
+<?php include '../includes/navbar.php'; ?>
+
+<div class="container">
+    <h1>High-Risk Operations</h1>
+    <p>DEVGRU is involved in some of the most high-stakes missions worldwide, often dealing with counter-terrorism and hostage rescue.</p>
+    <ul>
+        <li>Operation Neptune Spear - Targeted operation against high-profile terrorist leaders.</li>
+        <li>Operation Just Cause - Strategic operations in hostile territories.</li>
+        <li>Mission Infiltration - Deep reconnaissance in high-risk environments.</li>
+    </ul>
+</div>
+
+<?php include '../includes/footer.php'; ?>
