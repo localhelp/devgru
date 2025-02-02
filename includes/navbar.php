c696a3c6513437c5ea033566fdfd58829906a189
kazakh <166235011+localhelp@users.noreply.github.com>
2025-02-02 19:32:32 +0100

    Create navbar.php

---
+++ includes/navbar.php
@@ -0,0 +1,8 @@
+<nav>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="pages/operations.php">Operations</a></li>
+        <li><a href="pages/news.php">News</a></li>
+        <li><a href="pages/contact.php">Contact</a></li>
+    </ul>
+</nav>
