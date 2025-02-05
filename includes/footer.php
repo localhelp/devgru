f5d11873808f6bbce004897b22d002c89a372908
kazakh <166235011+localhelp@users.noreply.github.com>
2025-02-05 15:03:09 +0100

    Delete includes directory

--- includes/footer.php
+++
@@ -1,5 +0,0 @@
-<footer>
-    <p>&copy; 2025 DEVGRU. All Rights Reserved. All operations classified.</p>
-</footer>
-</body>
-</html>
--- includes/navbar.php
+++
@@ -1,8 +0,0 @@
-<nav>
-    <ul>
-        <li><a href="index.php">Home</a></li>
-        <li><a href="pages/operations.php">Operations</a></li>
-        <li><a href="pages/news.php">News</a></li>
-        <li><a href="pages/contact.php">Contact</a></li>
-    </ul>
-</nav>
